52a7d99c6edcb4923278f7016260fb33aaf5f856
Nikita Ryzhkin <timongo@bk.ru>
2022-01-24 21:11:34 +0400

    Initial Commit

---
+++ ColorMIX/ColorMIXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMIXApp.swift
+//  ColorMIX
+//
+//  Created by Никита Рыжкин on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorMIXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
